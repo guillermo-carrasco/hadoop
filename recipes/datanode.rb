4f1a0b4e428f66a7ee89e7a471f1cd9789d439ad
Guillermo Carrasco <guille.ch.88@gmail.com>
2012-11-12 13:05:04 +0100

    Created repo.rb recipe to add Cloudera's repository and included in all the other recipes

--- recipes/datanode.rb
+++ recipes/datanode.rb
@@ -17,29 +17,7 @@
 # limitations under the License.
 #
 
-include_recipe "java"
-
-execute "apt-get update" do
-  action :nothing
-end
-
-template "/etc/apt/sources.list.d/cloudera.list" do
-  owner "root"
-  mode "0644"
-  source "cloudera.list.erb"
-  notifies :run, resources("execute[apt-get update]"), :immediately
-end
-
-#Add correct key depending on the codename
-key_url = case node[:lsb][:codename]
-  when "lucid" then "curl -s http://archive.cloudera.com/cdh4/ubuntu/lucid/amd64/cdh/archive.key | sudo apt-key add -"
-  when "precise" then "curl -s http://archive.cloudera.com/cdh4/ubuntu/precise/amd64/cdh/archive.key | sudo apt-key add -"
-  when "squeeze" then "curl -s http://archive.cloudera.com/cdh4/debian/squeeze/amd64/cdh/archive.key | sudo apt-key add -"
-end
-
-execute key_url do
-  not_if "apt-key export 'Cloudera Apt Repository'"
-end
+include_recipe "hadoop::repo"
 
 package "hadoop"
 #Datanode specific packages
