518642be39ca052bca56ad4e13a7938121b18bda
Guillermo Carrasco <guille.ch.88@gmail.com>
2012-11-15 10:53:16 +0100

    Edited metadata

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
-maintainer        "Opscode, Inc."
-maintainer_email  "cookbooks@opscode.com"
+author            "Opscode, Inc."
+maintainer        "Guillermo Carrasco"
+maintainer_email  "guillermo.carrasco@scilifelab.se"
 license           "Apache 2.0"
 description       "Installs hadoop and sets up basic cluster per Cloudera's quick start docs"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -7,8 +8,8 @@ version           "0.8.1"
 depends           "java"
 
 recipe "hadoop", "Installs hadoop from Cloudera's repo"
-recipe "hadoop::conf_pseudo", "Installs hadoop-conf-pseudo and enables hadoop services"
-recipe "hadoop::doc", "Installs hadoop documentation"
+recipe "hadoop::naenode", "Installs hadoop-namenode packages"
+recipe "hadoop::datanode", "Installs hadoop-datanode packages"
 recipe "hadoop::hive", "Installs hadoop's hive package"
 recipe "hadoop::pig", "Installs hadoop's pig package"
 
