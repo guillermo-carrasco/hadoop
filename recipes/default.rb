8fe0b1bdb5d878eb92fc7298ae92441a745fdc0a
Guillermo Carrasco <guille.ch.88@gmail.com>
2012-11-15 12:43:01 +0100

    Creation of the cofig directory

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,3 +7,10 @@
 include_recipe "hadoop::repo"
 
 package "hadoop"
+
+#Copy config directory (couldn't find a better way to do it)
+execute "Copy default config directory" do
+    command "cp -r /etc/hadoop/conf.empty /etc/hadoop/conf.my_cluster"
+    command "update-alternatives --install /etc/hadoop/conf hadoop-conf /etc/hadoop/conf.my_cluster/ 50"
+    command "update-alternatives --set hadoop-conf /etc/hadoop/conf.my_cluster"
+end
